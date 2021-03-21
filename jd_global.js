d748eb4188d019d5d696ec71f46e050dbcb5582a
lonma <704015141@qq.com>
2021-03-21 17:50:16 +0800

    Update jd_global.js

--- jd_global.js
+++ jd_global.js
@@ -306,7 +306,7 @@ function readShareCode() {
   console.log(`开始`)
   return new Promise(async resolve => {
     $.get({
-      url: `http://jd.turinglabs.net/api/v2/jd/jdglobal/read/${randomCount}/`,
+      url: `http://jd.turinglabs.net/api/v2/jd/jdgloba/read/${randomCount}/`,
       'timeout': 10000
     }, (err, resp, data) => {
       try {
